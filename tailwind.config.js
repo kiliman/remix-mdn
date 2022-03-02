e3ec8e405bde66f413f4749fde41e0be42562d59
kiliman <kiliman@gmail.com>
2022-03-02 17:26:23 -0500

    🔥 Remove Tailwind CSS

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['app/**/*.{tsx,ts,jsx,js}', 'content/**/*.{tsx,ts,jsx,js,md,mdx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
